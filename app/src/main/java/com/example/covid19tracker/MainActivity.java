0ee47ee0bcb0f0e16c909fca4707ed54d0a561df
ashmit35 <ashmit.rock35@gmail.com>
2023-07-17 22:36:12 +0530

    initial commit

---
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.covid19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Model> data = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(MainActivity.this,data);
+        recyclerView.setAdapter(adapter);
+        retrieveData();
+
+        Button searchBtn = findViewById(R.id.search);
+        EditText tv = findViewById(R.id.search_box);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stateName = tv.getText().toString();
+                tv.setText("");
+                filterState(stateName);
+            }
+        });
+    }
+    private void retrieveData(){
+        final String URL = "https://data.covid19india.org/state_district_wise.json";
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL,null,new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Toast.makeText(MainActivity.this, "Inside on response", Toast.LENGTH_SHORT).show();
+                    Iterator<String> keys = response.keys();
+                    keys.next();
+
+                    while (keys.hasNext()) {
+
+                        String stateName = keys.next();
+                        JSONObject object = response.getJSONObject(stateName);
+                        JSONObject cityData = object.getJSONObject("districtData");
+                        Iterator<String> cities = cityData.keys();
+
+                        while (cities.hasNext()) {
+
+                            String cityName = cities.next();
+                            JSONObject city_data = cityData.getJSONObject(cityName);
+                            long active = city_data.getLong("active");
+                            long recovered = city_data.getLong("recovered");
+                            long deceased = city_data.getLong("deceased");
+                            long confirmed = city_data.getLong("confirmed");
+
+                            data.add(new Model(Long.toString(confirmed), Long.toString(active), Long.toString(recovered), Long.toString(deceased), stateName, cityName));
+                        }
+
+                        adapter.notifyDataSetChanged();
+                    }
+
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(request);
+    }
+    private void filterState(String state){
+        ArrayList<Model> filteredList = new ArrayList<>();
+
+        for(Model item:data){
+            if(item.getState().toLowerCase().contains(state.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+
+        if(filteredList.isEmpty()) Toast.makeText(this, "No state found by this name.", Toast.LENGTH_SHORT).show();
+        else{
+
+            Dialog dialogBox = new Dialog(this);
+            dialogBox.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialogBox.setContentView(R.layout.dialogue_box_layout);
+
+            RecyclerView dialog_recycler = dialogBox.getWindow().findViewById(R.id.dialog_recycler);
+            Adapter newAdapter = new Adapter(this,filteredList);
+            dialog_recycler.setLayoutManager(new LinearLayoutManager(this));
+            dialog_recycler.setAdapter(newAdapter);
+            dialogBox.show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/covid19tracker/Model.java
@@ -0,0 +1,95 @@
+package com.example.covid19tracker;
+
+public class Model {
+    private String confirmed,active,deceased,recovered,delta_confirmed,delta_deceased,delta_recovered;
+    private String state,city,notes;
+
+    public String getConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(String confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+    public String getDeceased() {
+        return deceased;
+    }
+
+    public void setDeceased(String deceased) {
+        this.deceased = deceased;
+    }
+
+    public String getRecovered() {
+        return recovered;
+    }
+
+    public void setRecovered(String recovered) {
+        this.recovered = recovered;
+    }
+
+    public String getDelta_confirmed() {
+        return delta_confirmed;
+    }
+
+    public void setDelta_confirmed(String delta_confirmed) {
+        this.delta_confirmed = delta_confirmed;
+    }
+
+    public String getDelta_deceased() {
+        return delta_deceased;
+    }
+
+    public void setDelta_deceased(String delta_deceased) {
+        this.delta_deceased = delta_deceased;
+    }
+
+    public String getDelta_recovered() {
+        return delta_recovered;
+    }
+
+    public void setDelta_recovered(String delta_recovered) {
+        this.delta_recovered = delta_recovered;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Model(String confirmed, String active, String recovered,String deceased,String state, String city) {
+        this.confirmed = confirmed;
+        this.active = active;
+        this.deceased = deceased;
+        this.recovered = recovered;
+        this.state = state;
+        this.city = city;
+    }
+}
